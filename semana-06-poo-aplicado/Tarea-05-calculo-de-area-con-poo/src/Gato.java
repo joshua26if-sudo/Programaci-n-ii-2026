089192b35fe7866d24a3b2d65c2db6cfc0252283
joshua26if-sudo <joshua26if@gmail.com>
2026-08-23 14:56:30 -0600

    Actualizacion de Archivos

---
+++ semana-06-poo-aplicado/Tarea-05-calculo-de-area-con-poo/src/Gato.java
@@ -0,0 +1,35 @@
+public class Gato extends Mascota {
+
+    private boolean esInterior;
+
+    public Gato(
+            String codigoPaciente,
+            String nombre,
+            int edadMeses,
+            double pesoKg,
+            boolean esInterior) {
+
+        super(codigoPaciente, nombre, edadMeses, pesoKg);
+        this.esInterior = esInterior;
+    }
+
+    public boolean isEsInterior() {
+        return esInterior;
+    }
+
+    @Override
+    public void emitirSonido() {
+        System.out.println(
+                getNombre() + ": ¡Miau!");
+    }
+
+    @Override
+    public double calcularCostoConsulta() {
+        double costo = 90.00;
+
+        if (!esInterior) {
+            costo += 15.00;
+        }
+        return costo;
+    }
+}
